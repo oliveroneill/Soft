a78476e30f3d8ca8d498d9810fa012a16d01e6da
Oliver <oliveroneill04@gmail.com>
2018-08-27 19:17:39 +1000

    Add tests for new functions

--- Tests/SoftTests/SpotifyClientTests.swift
+++ Tests/SoftTests/SpotifyClientTests.swift
@@ -633,4 +633,219 @@ final class SpotifyClientTests: XCTestCase {
             }
         }
     }
+
+
+    func testRelatedArtists() {
+        let images = [
+            Image(
+                url: "https://i.scdn.co/image/eb266625dab075341e8c4378a177a27370f91903",
+                width: 1000,
+                height: 816
+            )
+        ]
+        let expectedArtists = Artists(
+            artists:[
+                Artist(
+                    externalUrls: ["spotify" : "https://open.spotify.com/artist/0OdUWJ0sBjDrqHygGUXeCF"],
+                    followers: Followers(href: nil, total: 306565),
+                    genres: ["indie folk", "indie pop"],
+                    href: "https://api.spotify.com/v1/artists/0OdUWJ0sBjDrqHygGUXeCF",
+                    id: "0OdUWJ0sBjDrqHygGUXeCF",
+                    images: images,
+                    name: "Band of Horses",
+                    popularity: 59,
+                    uri: "spotify:artist:0OdUWJ0sBjDrqHygGUXeCF"
+                )
+            ]
+        )
+        let data = """
+{
+"artists": [
+{
+  "external_urls" : {
+    "spotify" : "https://open.spotify.com/artist/0OdUWJ0sBjDrqHygGUXeCF"
+  },
+  "followers" : {
+    "href" : null,
+    "total" : 306565
+  },
+  "genres" : [ "indie folk", "indie pop" ],
+  "href" : "https://api.spotify.com/v1/artists/0OdUWJ0sBjDrqHygGUXeCF",
+  "id" : "0OdUWJ0sBjDrqHygGUXeCF",
+  "images" : [ {
+    "height" : 816,
+    "url" : "https://i.scdn.co/image/eb266625dab075341e8c4378a177a27370f91903",
+    "width" : 1000
+  }],
+  "name" : "Band of Horses",
+  "popularity" : 59,
+  "type" : "artist",
+  "uri" : "spotify:artist:0OdUWJ0sBjDrqHygGUXeCF"
+}]
+}
+""".data(using: .utf8)!
+        let networkResponse: (Data?, HTTPURLResponse?, Error?) = (data, response, nil)
+        let artistID = "artist_1223"
+        let client = SpotifyClient(client: FakeClient(expected: networkResponse))
+        client.relatedArtists(artistID: artistID) {
+            switch $0 {
+            case .success(let artists):
+                XCTAssertEqual(expectedArtists, artists)
+            case .failure(let error):
+                XCTFail("Unexpected error: \(error)")
+            }
+        }
+    }
+
+    func testAlbum() {
+        let images = [
+            Image(
+                url: "https://i.scdn.co/image/07c323340e03e25a8e5dd5b9a8ec72b69c50089d",
+                width: 640, height: 640
+            )
+        ]
+        let artist = SimplifiedArtist(
+            externalUrls: ["spotify" : "https://open.spotify.com/artist/2BTZIqw0ntH9MvilQ3ewNY"],
+            href: "https://api.spotify.com/v1/artists/2BTZIqw0ntH9MvilQ3ewNY",
+            id: "2BTZIqw0ntH9MvilQ3ewNY",
+            name: "Cyndi Lauper",
+            uri: "spotify:artist:2BTZIqw0ntH9MvilQ3ewNY"
+        )
+        let simplifiedAlbum = SimplifiedAlbum(
+            artists: [artist], albumType: .single,
+            availableMarkets: ["AD"],
+            externalUrls: ["spotify": "https://open.spotify.com/album/0tGPJ0bkWOUmH7MEOR77qc"],
+            href: "https://api.spotify.com/v1/albums/0tGPJ0bkWOUmH7MEOR77qc",
+            id: "0tGPJ0bkWOUmH7MEOR77qc",
+            images: images, name: "Cut To The Feeling",
+            uri: "spotify:album:0tGPJ0bkWOUmH7MEOR77qc"
+        )
+        let track = SimplifiedTrack(
+            artists: [artist],
+            availableMarkets: ["AD"],
+            discNumber: 1,
+            durationMs: 305560,
+            explicit: false,
+            externalUrls: ["spotify" : "https://open.spotify.com/track/3f9zqUnrnIq0LANhmnaF0V"],
+            href: "https://api.spotify.com/v1/tracks/3f9zqUnrnIq0LANhmnaF0V",
+            id: "3f9zqUnrnIq0LANhmnaF0V",
+            name: "Money Changes Everything",
+            previewUrl: "https://p.scdn.co/mp3-preview/01bb2a6c9a89c05a4300aea427241b1719a26b06",
+            trackNumber: 1,
+            uri: "spotify:track:3f9zqUnrnIq0LANhmnaF0V"
+        )
+
+        let page = Page(
+            href: "https://api.spotify.com/v1/albums/0sNOF9WDwhWunNAHPD3Baj/tracks?offset=0&limit=50",
+            items: [track],
+            limit: 50,
+            next: nil,
+            offset: 0,
+            previous: nil,
+            total: 13
+        )
+        let expectedAlbum = Album(
+            artists: [artist],
+            albumType: .album,
+            availableMarkets: ["AD"],
+            copyrights: [["text" : "(P) 2000 Sony Music Entertainment Inc.", "type" : "P"]],
+            externalIds: ["upc" : "5099749994324"],
+            externalUrls: ["spotify" : "https://open.spotify.com/album/0sNOF9WDwhWunNAHPD3Baj"],
+            genres: [],
+            href: "https://api.spotify.com/v1/albums/0sNOF9WDwhWunNAHPD3Baj",
+            id: "0sNOF9WDwhWunNAHPD3Baj",
+            images: images,
+            name: "She's So Unusual",
+            popularity: 39,
+            releaseDate: "1983",
+            releaseDatePrecision: "year",
+            tracks: page,
+            uri: "spotify:album:0sNOF9WDwhWunNAHPD3Baj"
+        )
+        let data = """
+{
+  "album_type" : "album",
+  "artists" : [ {
+    "external_urls" : {
+      "spotify" : "https://open.spotify.com/artist/2BTZIqw0ntH9MvilQ3ewNY"
+    },
+    "href" : "https://api.spotify.com/v1/artists/2BTZIqw0ntH9MvilQ3ewNY",
+    "id" : "2BTZIqw0ntH9MvilQ3ewNY",
+    "name" : "Cyndi Lauper",
+    "type" : "artist",
+    "uri" : "spotify:artist:2BTZIqw0ntH9MvilQ3ewNY"
+  } ],
+  "available_markets" : [ "AD" ],
+  "copyrights" : [ {
+    "text" : "(P) 2000 Sony Music Entertainment Inc.",
+    "type" : "P"
+  } ],
+  "external_ids" : {
+    "upc" : "5099749994324"
+  },
+  "external_urls" : {
+    "spotify" : "https://open.spotify.com/album/0sNOF9WDwhWunNAHPD3Baj"
+  },
+  "genres" : [ ],
+  "href" : "https://api.spotify.com/v1/albums/0sNOF9WDwhWunNAHPD3Baj",
+  "id" : "0sNOF9WDwhWunNAHPD3Baj",
+  "images" : [ {
+    "height" : 640,
+    "url" : "https://i.scdn.co/image/07c323340e03e25a8e5dd5b9a8ec72b69c50089d",
+    "width" : 640
+  } ],
+  "name" : "She's So Unusual",
+  "popularity" : 39,
+  "release_date" : "1983",
+  "release_date_precision" : "year",
+  "tracks" : {
+    "href" : "https://api.spotify.com/v1/albums/0sNOF9WDwhWunNAHPD3Baj/tracks?offset=0&limit=50",
+    "items" : [ {
+      "artists" : [ {
+        "external_urls" : {
+          "spotify" : "https://open.spotify.com/artist/2BTZIqw0ntH9MvilQ3ewNY"
+        },
+        "href" : "https://api.spotify.com/v1/artists/2BTZIqw0ntH9MvilQ3ewNY",
+        "id" : "2BTZIqw0ntH9MvilQ3ewNY",
+        "name" : "Cyndi Lauper",
+        "type" : "artist",
+        "uri" : "spotify:artist:2BTZIqw0ntH9MvilQ3ewNY"
+      } ],
+      "available_markets" : [ "AD"  ],
+      "disc_number" : 1,
+      "duration_ms" : 305560,
+      "explicit" : false,
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/track/3f9zqUnrnIq0LANhmnaF0V"
+      },
+      "href" : "https://api.spotify.com/v1/tracks/3f9zqUnrnIq0LANhmnaF0V",
+      "id" : "3f9zqUnrnIq0LANhmnaF0V",
+      "name" : "Money Changes Everything",
+      "preview_url" : "https://p.scdn.co/mp3-preview/01bb2a6c9a89c05a4300aea427241b1719a26b06",
+      "track_number" : 1,
+      "type" : "track",
+      "uri" : "spotify:track:3f9zqUnrnIq0LANhmnaF0V"
+    } ],
+    "limit" : 50,
+    "next" : null,
+    "offset" : 0,
+    "previous" : null,
+    "total" : 13
+  },
+  "type" : "album",
+  "uri" : "spotify:album:0sNOF9WDwhWunNAHPD3Baj"
+}
+""".data(using: .utf8)!
+        let networkResponse: (Data?, HTTPURLResponse?, Error?) = (data, response, nil)
+        let albumID = "album_1223"
+        let client = SpotifyClient(client: FakeClient(expected: networkResponse))
+        client.album(albumID: albumID) {
+            switch $0 {
+            case .success(let album):
+                XCTAssertEqual(expectedAlbum, album)
+            case .failure(let error):
+                XCTFail("Unexpected error: \(error)")
+            }
+        }
+    }
 }
